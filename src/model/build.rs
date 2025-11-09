fab7a8d28774efbb595f1896654e8220b7166806
Marcus Weiner <marcus.weiner@gmail.com>
2025-11-09 20:52:29 +0100

    Add more fields to build model

--- src/model/build.rs
+++ src/model/build.rs
@@ -1,3 +1,5 @@
+use std::time;
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize)]
@@ -22,9 +24,13 @@ pub enum BuilderStatus {
 /// Represents a build that has started
 #[derive(Deserialize, Serialize)]
 pub struct Build {
+    pub request: super::build_req::BuildRequest,
+    pub request_hash: String,
+
     pub detail: GeneralStatus,
     pub imagebuilder_status: BuilderStatus,
-    pub request: super::build_req::BuildRequest,
+    pub enqueued_at: Option<time::SystemTime>,
+
     pub stdout: Option<String>,
     pub stderr: Option<String>,
     pub build_cmd: Option<String>,
